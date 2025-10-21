fd3ea54c078056fbf5d9646f9447540342a80c19
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-10-21 16:26:54 +0530

    add auth verify for next middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,25 @@ import type { NextRequest } from "next/server";
 const protectedRoutes = ["/home"];
 const authPages = ["/login", "/register"];
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     const url = req.nextUrl.clone();
-    const token = req.cookies.get("accessToken");
 
     const isProtected = protectedRoutes.some((path) => url.pathname.startsWith(path));
     const isAuthPage = authPages.some((path) => url.pathname.startsWith(path));
 
-    if (isProtected && !token) {
+    const verifyRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/verify`, {
+        headers: { cookie: req.headers.get("cookie") || "" },
+        credentials: "include",
+    });
+
+    const isAuthenticated = verifyRes.ok;
+
+
+    if (isProtected && !isAuthenticated) {
         url.pathname = "/login";
         return NextResponse.redirect(url);
     }
-    if (isAuthPage && token) {
+    if (isAuthPage && isAuthenticated) {
         url.pathname = "/home";
         return NextResponse.redirect(url);
     }
